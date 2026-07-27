5ae1aff808589a60c1f49eae1865b7972da7a53e
a-kashif-ahmed <kashifmsd7@gmail.com>
2026-07-27 16:57:18 +0530

    MERGED-rm 0.1

--- api/utils/get_parent_path.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-# Returns the parent directory of this file (i.e., utils directory’s parent, which is the project root)
-
-def get_parent_path() -> Path:
-    return Path(__file__).parent.parent
